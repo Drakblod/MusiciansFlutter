2cc7760cdcdb340963e3310196541b669b68004e
alex hillbom <alexhillbom@outlook.com>
2026-07-17 15:15:05 +0200

    Standardize profile views on ios-branch. Resolve unused import.

--- lib/views/profile_tab_screen.dart
+++ lib/views/profile_tab_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:url_launcher/url_launcher.dart';
 import '../providers/app_state.dart';
 import '../theme/app_theme.dart';
 import '../models/user_profile.dart';
