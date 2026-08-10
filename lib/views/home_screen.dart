9c5f216312e328e877a67e6b87736f3d69bc8574
alex hillbom <alexhillbom@outlook.com>
2026-08-11 01:21:03 +0200

    Bump version to 1.97.0+98

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -365,7 +365,7 @@ class HomeScreen extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.only(bottom: 10, right: 10),
                 child: Text(
-                  '1.96',
+                  '1.97',
                   style: GoogleFonts.inter(
                     color: AppTheme.textSecondary.withOpacity(0.5),
                     fontSize: 12,
@@ -1117,7 +1117,7 @@ class _ExperimentalHomeViewContentState extends State<ExperimentalHomeViewConten
                               }
                             },
                             child: Text(
-                              '1.96',
+                              '1.97',
                               style: GoogleFonts.inter(
                                 color: AppTheme.textSecondary.withOpacity(0.5),
                                 fontSize: 12,
